61e2b6002dfc130f7f8c6ddaf8f9d2d8e4ec652a
Felipe Rodrigues Queiroz <feliperodriguesqueiroz122@gmail.com>
2024-03-14 20:05:19 -0300

    first commit!

--- command-controller.go
+++ command-controller.go
@@ -4,7 +4,8 @@ type Name string
 
 const (
 	GITSTATUS   Name = "git status"
-	GITCOMMIT        = "git commit" // TODO "git commit -m"
+	GITCOMMIT        = "git commit"
+	GITCOMMITM	 = "git commit -m"
 	GITINIT          = "git init"
 	GITCLONE         = "git clone"
 	GITPUSH          = "git push"
@@ -16,6 +17,7 @@ const (
 var CommandMap = map[Name]Command{
 	GITSTATUS:   Command{"Comando utilizado para verificar as mudanças nos arquivos de um projeto."},
 	GITCOMMIT:   Command{"Comando utilizado para levar as mudanças de um ambiente local para um repositório git, com a opção de inserir uma mensagem descritiva (-m [mensagem])."},
+	GITCOMMITM:  Command{"Comando utilizado para salvar as alterações feitas em um repositório Git, o -m permite adicionar uma mensagem de commit junto ao comando, descrevendo assim as alterações feitas."},
 	GITINIT:     Command{"Comando utilizado para inicializar um repositório local."},
 	GITCLONE:    Command{"Comando utilizado para clonar um repositório remoto git para sua máquina."},
 	GITPUSH:     Command{"Comando utilizado para de fato enviar as mudanças locais para o repositório git."},
