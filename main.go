bfb95aec7f1cea2741cb662044282590da6f58a6
Marcos <marcos.lprado@sempreceub.com>
2024-03-14 13:28:30 -0300

    Estudos LTP2

--- main.go
+++ main.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+	"bufio"
+)
 func main(){
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Insert a git command: ")
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+	command, ok := CommandMap[Name(input)]
+	if !ok {
+		fmt.Printf("REPROVADO! SR-!!!!! VOCÊ REALMENTE ACHA QUE %v EXISTE? PODE TRANCAR!!!!!!\n", input)
+		return
+	}
+	
+	
+	fmt.Printf("%+v\n", command.description)
+	
+	
 }
