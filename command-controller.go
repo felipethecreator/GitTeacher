70745fe01e08ecb2ba6fcb731fff83ac2703a867
Gabriel Lima <gabrilima2004@gmail.com>
2024-03-14 14:28:15 -0300

    Addition of the git checkout command.

--- command-controller.go
+++ command-controller.go
@@ -3,21 +3,23 @@ package main
 type Name string
 
 const (
-	GITSTATUS Name = "git status"
-	GITCOMMIT = "git commit" // TODO "git commit -m" 
-	GITINIT = "git init"
-	GITCLONE = "git clone"
-	GITPUSH = "git push"
-	GITPULL = "git pull"
-	GITADD = "git add"
+	GITSTATUS   Name = "git status"
+	GITCOMMIT        = "git commit" // TODO "git commit -m"
+	GITINIT          = "git init"
+	GITCLONE         = "git clone"
+	GITPUSH          = "git push"
+	GITPULL          = "git pull"
+	GITADD           = "git add"
+	GITCHECKOUT      = "git checkout"
 )
 
-var CommandMap = map[Name]Command {
-	GITSTATUS : Command {"Comando utilizado para verificar as mudanças nos arquivos de um projeto"},
-	GITCOMMIT : Command {"Comando utilizado para levar as mudanças de um ambiente local para um repositório git, com a opção de inserir uma mensagem descritiva (-m [mensagem])"},
-	GITINIT : Command {"Comando utilizado para inicializar um repositório local"},
-	GITCLONE: Command {"Comando utilizado para clonar um repositório remoto git para sua máquina"},
-	GITPUSH : Command {"Comando utilizado para de fato enviar as mudanças locais para o repositório git"},
-	GITPULL : Command {"Comando utilizado para trazer as mudanças do repositório git para seu repositório local"},
-	GITADD : Command {"Comando utilizado para preparar arquivos para o commit"},
+var CommandMap = map[Name]Command{
+	GITSTATUS:   Command{"Comando utilizado para verificar as mudanças nos arquivos de um projeto."},
+	GITCOMMIT:   Command{"Comando utilizado para levar as mudanças de um ambiente local para um repositório git, com a opção de inserir uma mensagem descritiva (-m [mensagem])."},
+	GITINIT:     Command{"Comando utilizado para inicializar um repositório local."},
+	GITCLONE:    Command{"Comando utilizado para clonar um repositório remoto git para sua máquina."},
+	GITPUSH:     Command{"Comando utilizado para de fato enviar as mudanças locais para o repositório git."},
+	GITPULL:     Command{"Comando utilizado para trazer as mudanças do repositório git para seu repositório local."},
+	GITADD:      Command{"Comando utilizado para preparar arquivos para o commit."},
+	GITCHECKOUT: Command{"Comando utilizado para mudar para uma branch específica."},
 }
